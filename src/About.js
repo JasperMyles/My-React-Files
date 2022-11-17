3d4c28cea407b69668dfdcff248912fde8f7d77a
JasperMyles <ograhjoshua@gmail.com>
2022-11-17 13:03:59 +0100

    New commit on my work

--- src/About.js
+++ src/About.js
@@ -1,27 +1,31 @@
 import React from "react";
 
 const About = (props) => {
-  let myMap = props.newData;
-  return (
-    <div>
-      <div>
-        {myMap.map((each) => {
-          return (
-            <div>
-              <h1>{each.name.toUpperCase()}</h1>
-              <p>{each.status.toLowerCase()}</p>
-              <p>{each.age}</p>
-              <p>
-                {each.isMarried == true
-                  ? `${each.name.toUpperCase()} IS MARRIED`
-                  : ""}
-              </p>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+    let myMap = props.newData;
+    return ( <
+        div >
+        <
+        div > {
+            myMap.map((each) => {
+                return ( <
+                    div >
+                    <
+                    h1 > { each.name.toUpperCase() } < /h1> <
+                    p > { each.status.toLowerCase() } < /p> <
+                    p > { each.age } < /p> <
+                    p > {
+                        each.isMarried == true ?
+                        `${each.name.toUpperCase()} IS MARRIED` :
+                            `${each.name.toUpperCase()} IS NOT MARRIED`
+                    } <
+                    /p> <
+                    /div>
+                );
+            })
+        } <
+        /div> <
+        /div>
+    );
 };
 
 export default About;
